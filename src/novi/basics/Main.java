5d8c041f697540efbb82cc7e3a8cc88ec28b6a96
Lara Nettesheim <lara.nettesheim@gmail.com>
2020-06-02 11:35:07 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Lara");
+
+        // type         naam                // waarde --> object
+        LoginController loginController = new LoginController();
     }
 }
